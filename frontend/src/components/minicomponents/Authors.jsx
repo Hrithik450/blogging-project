3c53186b2e2f26bba34e1f89c7bc44f3d0081b55
Hruthik <mhrithik450@gmail.com>
2024-09-01 18:03:40 +0530

    Update Authors.jsx

--- frontend/src/components/minicomponents/Authors.jsx
+++ frontend/src/components/minicomponents/Authors.jsx
@@ -9,14 +9,14 @@ const Authors = () => {
     const FetchAuthors = async () => {
       try {
         const response = axios.get(
-          "http://localhost:7000/api/v1/user/authors",
+          "https://blog-backend-c8by.onrender.com/api/v1/user/authors",
           { withCredentials: true }
         );
         if ((await response).status === 200) {
           setauthors((await response).data.Authors);
         }
       } catch (error) {
-        console.log(error);
+        setauthors([])
       }
     };
 
